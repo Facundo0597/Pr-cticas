6f46431fd70046ebb23bc8c812f7f4bc03301583
Facundo Martin <facundo.0597@gmail.com>
2025-04-04 00:03:47 -0300

    Librerías

---
+++ práctica.py
@@ -0,0 +1,9 @@
+# Para importar libreria math
+import math
+# Se puede renombrar la libreria, la llamaremos "mt"
+import math as mt
+# Ejemplo de uso
+# sqrt se usa para calcular raiz cuadrada
+print(mt.sqrt(81))
+# Si nos dirigimos a la documentación de python figuran las librerías disponibles
+
